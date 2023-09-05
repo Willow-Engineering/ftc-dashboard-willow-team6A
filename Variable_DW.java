56d1a412bcd73ed3f4b81397eb3ab7527ea49e2d
DeVonte Will <devonte_will.stu@willowschoolnola.org>
2023-09-05 10:45:06 -0500

    Hello World first step

--- Variable_DW.java
+++ Variable_DW.java
@@ -2,11 +2,19 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+
 
 @TeleOp()
 public class Variables extends OpMode {
     @Override
     public void init() {
+
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+
         int teamNumber = 16072;
         double motorSpeed = 0.5;
         boolean touchSensorPressed = true;

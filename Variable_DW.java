ea426f02ce5002ac111049f3265411b06e7b6d91
DeVonte Will <devonte_will.stu@willowschoolnola.org>
2023-09-05 10:40:34 -0500

    Update Variable_DW.java

--- Variable_DW.java
+++ Variable_DW.java
@@ -4,12 +4,13 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp()
-public class PrimitiveTypes extends OpMode {
+public class Variables extends OpMode {
     @Override
     public void init() {
-        int teamNumber = 16072
-        double motorSpeed = 0.5
-        boolean touchSensorPressed = true
+        int teamNumber = 16072;
+        double motorSpeed = 0.5;
+        boolean touchSensorPressed = true;
+
         telemetry.addData("Team Number", teamNumber);
         telemetry.addData("Motor Speed", motorSpeed);
         telemetry.addData("Touch Sensor", touchSensorPressed);

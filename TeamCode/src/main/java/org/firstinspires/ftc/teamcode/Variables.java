9ea2e64eea115591cd72fc700a667a429d5f91e9
DeVonte Will <devonte_will.stu@willowschoolnola.org>
2023-09-07 11:01:34 -0500

    Fix dumby

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 
 

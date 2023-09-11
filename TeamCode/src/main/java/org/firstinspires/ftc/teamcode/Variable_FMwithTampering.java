06581be67ef291667d16b078f7fb35fa7b6333e5
Marigny Gill <marigny_gill.stu@willowschoolnola.org>
2023-09-11 10:14:15 -0500

    3.3 Excercises
    THis

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variable_FMwithTampering.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variable_FMwithTampering.java
@@ -29,10 +29,16 @@ public class Variable_FMwithTampering extends OpMode {
     public void loop() {
         telemetry.addData("Left Stick x", gamepad1.left_stick_x);
         telemetry.addData("Left Stick y", gamepad1.left_stick_y);
+        telemetry.addData("Right Stick x", gamepad1.right_stick_x);
+        telemetry.addData("Right Stick y", gamepad1.right_stick_y);
         telemetry.addData("A button", gamepad1.a);
+        telemetry.addData("B button", gamepad1.b);
+        telemetry.addData("Left Trigger", gamepad1.left_trigger);
+        telemetry.addData("Right Trigger", gamepad1.right_trigger);
         double speedForward = -gamepad1.left_stick_y / 2.0;
         telemetry.addData("speed Forward", speedForward);
-
+        telemetry.addData("Joystick Difference",(gamepad1.left_stick_y - gamepad1.right_stick_y));
+        telemetry.addData("Trigger Sum",(gamepad1.left_trigger + gamepad1.right_trigger));
         if(gamepad1.left_stick_y < 0) {
             telemetry.addData("Left Stick", " is Negative");
         }
